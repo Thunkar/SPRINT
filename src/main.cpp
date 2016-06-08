d1b8d6db2504c22c41b99cd5e272cedf61947072
Gregorio Juliana <gregojquiros@gmail.com>
2016-06-08 12:04:15 +0200

    Added tree representation and majority class extraction

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,25 @@
 # include <iostream>
 # include "csv.h"
 # include "math.h"
+# include <iomanip>
 
 
 using namespace std;
 using namespace io;
 
-const int size_y = 1000;
+const int size_y = 100;
 const int size_x = 9;
 const int n_classes = 2;
 const int max_tree_depth = 5;
-const double gini_threshold = 0.1;
+const double gini_threshold = 0.35;
+
+struct TreeNode {
+    string *splitCondition;
+    TreeNode *left; 
+    TreeNode *right; 
+};
+
+TreeNode* root;
 
 int divide(string **array, int start, int end, int column) {
     int left;
@@ -58,7 +67,7 @@ void quicksort(string **array, int start, int end, int column)
     }
 }
 
-double gini(string **data_set, int size_y, int split_row, int column) {
+double* gini(string **data_set, int size_y, int split_row, int column) {
     double **gini_matrix = new double*[2];
     for(int i = 0; i < 2; i++) {
         gini_matrix[i] = new double[n_classes];
@@ -69,21 +78,31 @@ double gini(string **data_set, int size_y, int split_row, int column) {
     double total_yes = 0;
     double total_no = 0;
 
+    double *class_rank = new double[n_classes];
+
     for(int i = 0; i < size_y; i++) {
+        int class_id = atoi(data_set[i][0].c_str());
+        class_rank[class_id]++;
         if(i <= split_row){
-            gini_matrix[0][atoi(data_set[i][0].c_str())]++;
+            gini_matrix[0][class_id]++;
             total_yes++;
         }
         else {
-            gini_matrix[1][atoi(data_set[i][0].c_str())]++;
+            gini_matrix[1][class_id]++;
             total_no++;
         }
     }
 
     double gini_yes = 0;
     double gini_no = 0;
+    int majority_class = -1;
+    int max_class_occurences = -1;
 
     for(int j = 0; j < n_classes; j++){
+        if(class_rank[j] > max_class_occurences){
+            max_class_occurences = class_rank[j];
+            majority_class = j;
+        }
         gini_yes += (gini_matrix[0][j]/total_yes)*(gini_matrix[0][j]/total_yes);
         gini_no += (gini_matrix[1][j]/total_no)*(gini_matrix[1][j]/total_no);
     }
@@ -94,7 +113,7 @@ double gini(string **data_set, int size_y, int split_row, int column) {
         gini_no = 0;
     if(total_yes == 0)
         gini_yes = 0;
-    return gini_yes + gini_no;
+    return new double[2]{ gini_yes + gini_no, majority_class };
 
 }
 
@@ -102,29 +121,46 @@ string* bestGiniSplit(string ** data_set, int size_y) {
     double best_gini = 1;
     int row_split, column;
     string value;
+    double majority_class = -1;
     for(int j = 1; j < size_x; j++) {
         quicksort(data_set, 0, size_y - 1, j);
         for(int i = 0; i < size_y; i++){
-            double current_gini = gini(data_set, size_y, i, j);
-            if(current_gini <= best_gini){
-                best_gini = current_gini;
+            double* current_gini = gini(data_set, size_y, i, j);
+            if(current_gini[0] <= best_gini){
+                best_gini = current_gini[0];
                 column = j; 
                 row_split = i;
                 value = data_set[i][j];
+                majority_class = current_gini[1];
             }
         } 
     } 
-    return new string[4]{ to_string(row_split), to_string(column), to_string(best_gini), value }; 
+    return new string[5]{ to_string(row_split), to_string(column), to_string(best_gini), value, to_string(majority_class) }; 
 }
 
-void SPRINT(string **data_set, int size_y) {
+void printTree(TreeNode* p, int indent){
+    if(p != NULL) {
+        if(p->right) {
+            printTree(p->right, indent+4);
+        }
+        if (indent) {
+            cout << setw(indent) << ' ';
+        }
+        if (p->right) cout<<" /\n" << setw(indent) << ' ';
+        cout<< "Col: " << p->splitCondition[1] << " <= " << p->splitCondition[3] << " Class: " << p-> splitCondition[4] << endl;
+        if(p->left) {
+            cout << setw(indent) << ' ' <<" \\\n";
+            printTree(p->left, indent+4);
+        }
+    }
+}
+
+void SPRINT(string **data_set, int size_y, TreeNode* parent) {
     string *best_split = bestGiniSplit(data_set, size_y);
     int row_split = atoi(best_split[0].c_str());
     int column = atoi(best_split[1].c_str());
     double gini = atof(best_split[2].c_str());
     string value = best_split[3];
-
-    cout << "Column: " << column  << " Gini: " << gini << " Value: " << value << endl;
 
     if(gini < gini_threshold) return;
 
@@ -147,9 +183,22 @@ void SPRINT(string **data_set, int size_y) {
             right_leaf[i][j] = data_set[i+row_split][j];
         }
     }
-    SPRINT(left_leaf, left_size_y);
-    SPRINT(right_leaf, right_size_y);
+
+    TreeNode* node = new TreeNode();
+    *node = { best_split, nullptr, nullptr, };
+    if(parent){
+        if(parent->left)
+            parent->right = node;
+        else
+            parent->left = node;
+    } else {
+        root = node;
+    }
+
+    SPRINT(left_leaf, left_size_y, node);
+    SPRINT(right_leaf, right_size_y, node);
 }
+
 
 int main(int argc, char *argv[]){
 
@@ -167,8 +216,7 @@ int main(int argc, char *argv[]){
             data_set[i][j] = line[j];
         }
     }
-
-    SPRINT(data_set, size_y);
-
+    SPRINT(data_set, size_y, nullptr);
+    printTree(root, 0);
 }
 
